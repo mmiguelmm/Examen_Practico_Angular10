a0887e0547813a9fd551f362587d2b80ea7705ed
mmiguelmm <mmirandm@everis.com>
2021-02-13 11:25:36 -0500

    feat(1e):Crear una página de control de errores 404

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NotFoundPageComponent],
   imports: [
     CommonModule
   ]
